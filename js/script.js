b4b4f3c2d176b5afb56871ce5ab371b1cf3fb201
DVolodymyr <78636394+DVolodymyr@users.noreply.github.com>
2021-05-16 21:46:11 +0300

    addet TechTask & shop & acordion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//======================          TABS            ==========================================
 function openTab(tabName) {
   const tab = document.getElementById(tabName);
   const tabs = document.getElementsByClassName(`tabs__block`);
@@ -6,3 +7,18 @@ function openTab(tabName) {
   }
   tab.classList.add(`active`);
 }
+//======================           ACORDION          =======================================
+let acc = document.getElementsByClassName("accordion");
+let i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+//======================                      ==========================================
