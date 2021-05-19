944ca42210a8292c2b26afd64499dfd871967e75
DVolodymyr <78636394+DVolodymyr@users.noreply.github.com>
2021-05-19 18:05:08 +0300

    update & smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 //======================          TABS            ==========================================
 function openTab(tabName) {
   const tab = document.getElementById(tabName);
@@ -21,4 +22,28 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-//======================                      ==========================================
+//======================          SMOOTH SCROLL            ==========================================
+const menuLinks = document.querySelectorAll(".menu__item[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector("header").offsetHeight;
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      e.preventDefault();
+    }
+  }
+}
